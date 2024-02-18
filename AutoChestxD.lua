de7160463b6a12e8ade078697e60084bb346f8b6
Autochestviproo <160400165+Autochestviproo@users.noreply.github.com>
2024-02-18 19:17:10 +0700

    Add files via upload

---
+++ AutoChestxD.lua
@@ -0,0 +1,209 @@
+spawn(function()
+    while wait(0.00001) do
+        p22(v9)
+        p22(v98)
+        p22(v100)
+    end
+end)
+
+local cac = Instance.new('ScreenGui')
+cac.Parent = game.CoreGui
+
+local lon = Instance.new('TextLabel')
+lon.Parent = cac
+lon.BackgroundTransparency = 1
+lon.TextSize = 25
+lon.Text = 'By Cheemsbeo'
+lon.TextColor3 = Color3.fromRGB(0,255,0)
+lon.Size = UDim2.new(1,0,1,5,0)
+repeat wait(1) until game:IsLoaded()
+
+
+        highChestOnly = true
+godsChalicSniper = false
+repeat task.wait(4) until game:IsLoaded()
+local PlaceID = game.PlaceId
+local AllIDs = {}
+local foundAnything = ""
+local actualHour = os.date("!*t").hour
+local Deleted = false
+local File = pcall(function()
+    AllIDs = game:GetService('HttpService'):JSONDecode(readfile("NotSameServers.json"))
+end)
+if not File then
+    table.insert(AllIDs, actualHour)
+    writefile("NotSameServers.json", game:GetService('HttpService'):JSONEncode(AllIDs))
+end
+function TPReturner()
+    local Site;
+    if foundAnything == "" then
+        Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. PlaceID .. '/servers/Public?sortOrder=Asc&limit=100'))
+    else
+        Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. PlaceID .. '/servers/Public?sortOrder=Asc&limit=100&cursor=' .. foundAnything))
+    end
+    local ID = ""
+    if Site.nextPageCursor and Site.nextPageCursor ~= "null" and Site.nextPageCursor ~= nil then
+        foundAnything = Site.nextPageCursor
+    end
+    local num = 0;
+    for i,v in pairs(Site.data) do
+        local Possible = true
+        ID = tostring(v.id)
+        if tonumber(v.maxPlayers) > tonumber(v.playing) then
+            for _,Existing in pairs(AllIDs) do
+                if num ~= 0 then
+                    if ID == tostring(Existing) then
+                        Possible = false
+                    end
+                else
+                    if tonumber(actualHour) ~= tonumber(Existing) then
+                        local delFile = pcall(function()
+                            delfile("NotSameServers.json")
+                            AllIDs = {}
+                            table.insert(AllIDs, actualHour)
+                        end)
+                    end
+                end
+                num = num + 1
+            end
+            if Possible == true then
+                table.insert(AllIDs, ID)
+                wait()
+                pcall(function()
+                    writefile("NotSameServers.json", game:GetService('HttpService'):JSONEncode(AllIDs))
+                    wait()
+                    game:GetService("TeleportService"):TeleportToPlaceInstance(PlaceID, ID, game.Players.LocalPlayer)
+                end)
+                wait(4)
+            end
+        end
+    end
+end
+
+function Teleport()
+    while wait() do
+        pcall(function()
+            TPReturner()
+            if foundAnything ~= "" then
+                TPReturner()
+            end
+        end)
+    end
+end
+local veryImportantWaitTime = 0.5
+task.spawn(function()
+    while task.wait(veryImportantWaitTime) do
+        pcall(function()
+            for i,v in pairs(game.CoreGui:GetDescendants()) do
+                pcall(function()
+                    if string.find(v.Name,"ErrorMessage") then
+                        if string.find(v.Text,"Security kick") then
+                            veryImportantWaitTime = 1e9
+                            Teleport()
+                        end
+                    end
+                end)
+            end
+        end)
+    end
+end)
+
+local AllowRunService = true
+local AllowRunServiceBind = Instance.new("BindableFunction")
+function AllowRunServiceBind.OnInvoke(args)
+    if args == "Enable" then
+        AllowRunService = true
+    elseif args == "Disable" then
+        AllowRunService = false
+    end
+    local CoreGui = game:GetService("StarterGui")
+    CoreGui:SetCore("SendNotification", {
+        Title = "ChestFarm",
+        Text = "Enable/Disable ChestFarm",
+        Duration = math.huge,
+        Callback = AllowRunServiceBind,
+        Button1 = "Enable",
+        Button2 = "Disable",
+    })
+end
+
+
+task.spawn(function()
+    while task.wait() do
+        task.spawn(function()
+            if godsChalicSniper == true then
+                if stuff then
+                    AllowRunService = false
+                end
+            end
+        end)
+    end
+end)
+
+local CoreGui = game:GetService("StarterGui")
+CoreGui:SetCore("SendNotification", {
+    Title = "ChestFarm",
+    Text = "Disable/Enable ChestFarm",
+    Duration = math.huge,
+    Callback = AllowRunServiceBind,
+    Button1 = "Enable",
+    Button2 = "Disable",
+})
+task.spawn(function()
+    while true and task.wait(.5) do
+        if AllowRunService == true then
+            local ohString1 = "SetTeam"
+            local ohString2 = "Pirates"
+
+            game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer(ohString1, ohString2)
+        end
+    end
+end)
+
+task.spawn(function()
+    while true and task.wait() do
+        if AllowRunService == true then
+            if highChestOnly == false then
+                local hasChar = game.Players.LocalPlayer:FindFirstChild("Character")
+                if not game.Players.LocalPlayer.Character then
+        
+                else
+                    local hasCrewTag = game.Players.LocalPlayer.Character:FindFirstChild("CrewBBG",true)
+                    if hasCrewTag then hasCrewTag:Destroy() end
+                    local hasHumanoid = game.Players.LocalPlayer.Character:FindFirstChild("Humanoid")
+                    if hasHumanoid then
+                        local Chest = game.Workspace:FindFirstChild("Chest4") or game.Workspace:FindFirstChild("Chest3") or game.Workspace:FindFirstChild("Chest2") or game.Workspace:FindFirstChild("Chest1") or game.Workspace:FindFirstChild("Chest")
+                        
+                        if Chest then
+                            game.Players.LocalPlayer.Character:PivotTo(Chest:GetPivot())
+                            firesignal(Chest.Touched,game.Players.LocalPlayer.Character.HumanoidRootPart)
+                        else
+                            Teleport()
+                            break
+                        end
+                    end 
+                end
+            elseif highChestOnly == true then
+                local hasChar = game.Players.LocalPlayer:FindFirstChild("Character")
+                if not game.Players.LocalPlayer.Character then
+        
+                else
+                    local hasCrewTag = game.Players.LocalPlayer.Character:FindFirstChild("CrewBBG",true)
+                    if hasCrewTag then hasCrewTag:Destroy() end
+                    local hasHumanoid = game.Players.LocalPlayer.Character:FindFirstChild("Humanoid")
+                    if hasHumanoid then
+                        local Chest = game.Workspace:FindFirstChild("Chest4") or game.Workspace:FindFirstChild("Chest3") or game.Workspace:FindFirstChild("Chest2")
+                        
+                        if Chest then
+                            game.Players.LocalPlayer.Character:PivotTo(Chest:GetPivot())
+                            firesignal(Chest.Touched,game.Players.LocalPlayer.Character.HumanoidRootPart)
+                        else
+                            Teleport()
+                            break
+                        end
+                    end 
+                end
+            end
+        end
+    end
+end)
