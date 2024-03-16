3b6717f60a2a0ef92160fa0eed502312f9dc1851
loxus786 <114575575+loxus786@users.noreply.github.com>
2024-03-16 10:34:59 +0800

    Update AutoChestxD.lua

--- AutoChestxD.lua
+++ AutoChestxD.lua
@@ -19,7 +19,7 @@ lon.Size = UDim2.new(1,0,1,5,0)
 repeat wait(1) until game:IsLoaded()
 
 
-        highChestOnly = true
+        highChestOnly = false
 godsChalicSniper = true
 repeat task.wait(4) until game:IsLoaded()
 local PlaceID = game.PlaceId
